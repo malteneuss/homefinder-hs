241947956c89b8f6a79da87107d6b6be2db9628e
Malte Neuss <malteneuss@users.noreply.github.com>
2024-03-21 21:55:16 +0100

    Fix basic listing layout

--- src/Widget/HomeListItem.hs
+++ src/Widget/HomeListItem.hs
@@ -8,10 +8,10 @@ import Foundation (Widget)
 import Yesod (whamletFile)
 
 data HomeListItem = HomeListItem
-    { imageUrl :: [Text]
+    { imageUrl :: Maybe Text
     , title :: Text
     , address :: Text
-    , roomCount :: Text
+    , rooms :: Text
     , sqmLivingSpace :: Text
     , sqmPropertySpace :: Text
     , bedrooms :: Text
@@ -27,20 +27,20 @@ data HomeListItem = HomeListItem
 fakeHomeListItem :: HomeListItem
 fakeHomeListItem =
     HomeListItem
-        { imageUrl = ["https://example.com/image.jpg"]
+        { imageUrl = Just "/static/test/test-home-1.webp"
         , title = "Schönes Haus in den Vororten"
         , address = "123 Hauptstraße, 10007 Berlin"
-        , roomCount = "4"
-        , sqmLivingSpace = "150 sqm"
-        , sqmPropertySpace = "300 sqm"
-        , bedrooms = "3 beds"
-        , bathrooms = "2 baths"
+        , rooms = "4 Zimmer"
+        , sqmLivingSpace = "150 qm"
+        , sqmPropertySpace = "300 qm"
+        , bedrooms = "3 Bettzimmer"
+        , bathrooms = "2 Badezimmer"
         , price = "500.000€"
         , details = "Geräumiger Hinterhof, moderne Küche"
         , originalSource = "<html> ....</html>"
         , sourceUrl = "https://example.com/listing/123"
-        , firstFetchDate = "2022-01-01"
-        , lastFetchDate = "2022-01-05"
+        , firstFetchDate = "seit 2022-01-01"
+        , lastFetchDate = "vom 2022-02-05"
         }
 
 mkHomeListItemWidget :: HomeListItem -> Widget
