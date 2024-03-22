7182621ff8e4c45b7deb41b54795834f819eca61
Malte Neuss <malteneuss@users.noreply.github.com>
2024-03-22 19:41:38 +0100

    Move fake data into Home page

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -11,3 +11,23 @@ getHomeR = defaultLayout $ do
     setTitle "Homes"
     let homeListItems = replicate 10 $ mkHomeListItemWidget fakeHomeListItem
     $(whamletFile "templates/page-home.hamlet")
+
+fakeHomeListItem :: HomeListItem
+fakeHomeListItem =
+    HomeListItem
+        { imageUrl = Just "/static/test/test-home-1.webp"
+        , title = "Schönes Haus in den Vororten"
+        , address = "123 Hauptstraße, 10007 Berlin"
+        , rooms = "4 Zimmer"
+        , sqmLivingSpace = "150 qm"
+        , sqmPropertySpace = "300 qm"
+        , bedrooms = "3 Bettzimmer"
+        , bathrooms = "2 Badezimmer"
+        , price = "500.000€"
+        , details = "Geräumiger Hinterhof, moderne Küche"
+        , originalSource = "<html> ....</html>"
+        , sourceUrl = "https://example.com/listing/123"
+        , firstFetchDate = "seit 2022-01-01"
+        , lastFetchDate = "vom 2022-02-05"
+        }
+        
--- src/Widget/HomeListItem.hs
+++ src/Widget/HomeListItem.hs
@@ -24,24 +24,6 @@ data HomeListItem = HomeListItem
     , lastFetchDate :: Text
     }
 
-fakeHomeListItem :: HomeListItem
-fakeHomeListItem =
-    HomeListItem
-        { imageUrl = Just "/static/test/test-home-1.webp"
-        , title = "Schönes Haus in den Vororten"
-        , address = "123 Hauptstraße, 10007 Berlin"
-        , rooms = "4 Zimmer"
-        , sqmLivingSpace = "150 qm"
-        , sqmPropertySpace = "300 qm"
-        , bedrooms = "3 Bettzimmer"
-        , bathrooms = "2 Badezimmer"
-        , price = "500.000€"
-        , details = "Geräumiger Hinterhof, moderne Küche"
-        , originalSource = "<html> ....</html>"
-        , sourceUrl = "https://example.com/listing/123"
-        , firstFetchDate = "seit 2022-01-01"
-        , lastFetchDate = "vom 2022-02-05"
-        }
 
 mkHomeListItemWidget :: HomeListItem -> Widget
 mkHomeListItemWidget home = $(whamletFile "templates/widget-home-list-item.hamlet")
